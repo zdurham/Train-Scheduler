8a6d8c12de2f3db74710431d5880ef570382730b
Zach Durham <ZachDurham@dhcp-v80-100294.wireless.unc.edu>
2017-07-05 12:07:42 -0400

    added new <td> function and firebase obj

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,56 +1,52 @@
-$(document).ready(function () {
+
   // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAD41EfetxXQnPE3_OXtEqiek4zvU8ucvQ",
-    authDomain: "anton-e3e61.firebaseapp.com",
-    databaseURL: "https://anton-e3e61.firebaseio.com",
-    projectId: "anton-e3e61",
-    storageBucket: "anton-e3e61.appspot.com",
-    messagingSenderId: "1021309951045"
-  };
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyAD41EfetxXQnPE3_OXtEqiek4zvU8ucvQ",
+  authDomain: "anton-e3e61.firebaseapp.com",
+  databaseURL: "https://anton-e3e61.firebaseio.com",
+  projectId: "anton-e3e61",
+  storageBucket: "anton-e3e61.appspot.com",
+  messagingSenderId: "1021309951045"
+};
 
-  var database = firebase.database();
+firebase.initializeApp(config);
 
-  // Declare user inputs as variables
+var database = firebase.database();
+
+// Create a function to create a new cell, as we will be doing this multiple times per row
+function createTd(string, row) {
+  var td = $("<td>")
+  td.html(string)
+  $(row).append(td);
+}
+
+
+// Create a function that creates a row and adds each cell to the row. This takes the object (getting this from the database) and the key (unique for each row from database object)
+function buildRow(object, key) {
+
+}
   
 
-  var submit = $("#submit")
+ 
   // On click function that takes the data the user puts into the display fields
-  submit.on("click", function() {
-    var trainName = $("#train-name").val().trim();
-    var destination = $("#destination").val().trim();
-    var firstTime = $("#first-time").val().trim();
-    var frequency = $("#frequency").val().trim();
+$("#submit").on("click", function(e) {
+  // Prevent the default action
+  e.preventDefault();
 
-    // Calculate the Next Arrival and Minutes Away
-    
+  // Create an object that will be pushed to Firebase
+  var trainObj = {};
+  // Declare the properties of the object to match inputs
+  trainObj.name = $("#train-name").val().trim()
+  trainObj.destination = $("#destination").val().trim()
+  trainObj.firstTime = $("#first-time").val().trim()
+  trainObj.frequency = $("#frequency").val().trim()
 
-    console.log(trainName)
-    console.log(destination)
-    console.log(firstTime)
-    console.log(frequency)
-    
-    // Store the values in the database
-    database.ref().set({
-      trainName: trainName,
-      destination: destination,
-      firstTime: firstTime,
-      frequency: frequency
-    })
-
-    
-    
-    
-
-
-    // Clear the input fields
-    $("#train-name").val('')
-    $("#destination").val('')
-    $("#first-time").val('')
-    $("#frequency").val('')
+  // Console.log to test object
+  console.log(trainObj)
+  
+  // Push the object to Firebase 
+  database.ref().push(trainObj)
+  // Clear the input fields of the form
+  document.getElementById("add-train").reset();
 
   })
-  
-  
-})
